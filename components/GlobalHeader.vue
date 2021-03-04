628a391def2bc04a2947a2436bead2d3178eb66b
nullworkbench <nullworkbench@gmail.com>
2021-03-04 15:10:05 +0900

    create GlobalHeader.vue

---
+++ components/GlobalHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <ul>
+      <li><nuxt-link to="/">Top</nuxt-link></li>
+      <li><nuxt-link to="/works">Works</nuxt-link></li>
+      <li><nuxt-link to="/media">Media</nuxt-link></li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+div {
+  display: block;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GlobalHeader />
     <Nuxt />
   </div>
 </template>
